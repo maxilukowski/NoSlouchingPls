1e37442be68072c60107b53deda9a7f03cdab984
Maximilian Lukowski <m.lukowski@gentics.com>
2021-05-28 15:32:07 +0200

    Add swipe

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
-//import 'react-native-gesture-handler'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import 'react-native-gesture-handler'
 import Pomodoro from './pages/Pomodoro'
 import Home from './pages/Home'
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator()
 
 const App = () => {
   return (
-  <NavigationContainer>
-    <Tab.Navigator>
+  <NavigationContainer >
+    <Tab.Navigator tabBarPosition="bottom">
         <Tab.Screen name='Home' component={Home} />
         <Tab.Screen name='Pomodoro' component={Pomodoro} />
     </Tab.Navigator>
