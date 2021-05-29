25129173020fd8cabaf2c49440b12b8ee6d54307
maximilian <maxi.lukowski@yahoo.com>
2021-05-29 16:22:19 +0200

    Add timer logic and buttons

--- components/TomatenTimer.js
+++ components/TomatenTimer.js
@@ -1,36 +1,36 @@
-import React, { useState, useRef } from "react"
-import { Button, Text, View } from "react-native"
+import React, { useState, useRef } from 'react'
+import { Button, Text, View } from 'react-native'
 
-const TomatenTimer = ()=>{
-    const [timer, setTimer]=useState(0)
-    const [isActive, setIsActive]=useState(false)
-    const [isPaused, setIsPaused]=useState(false)
-    const countRef = useRef(null)
-    return(
-        <View>
-            <Text>{timer}</Text>
-            <Button onPress={handleStart} title="start"/>
-            <Button onPress={handlePause} title="pause"/>
-            <Button onPress={handleResume} title="resume"/>
-            <Button onPress={handleReset} title="reset"/>
-        </View>
-    )
-    function handleStart(){
-        setIsActive(true)
-        setIsPaused(true)
-        countRef.current = setInterval(()=>{
-            setTimer((timer)=>timer+1)
-        },1000)
+const TomatenTimer = () => {
+  const [timer, setTimer] = useState(0)
+  const [isPaused, setIsPaused] = useState(true)
+  const countRef = useRef()
+  return (
+    <View>
+      <Text>{timer}</Text>
+      <Button onPress={handleStart} title='start' />
+      <Button onPress={handlePause} title='pause' />
+      <Button onPress={handleReset} title='reset' />
+    </View>
+  )
+  function handleStart() {
+    if (isPaused) {
+      countRef.current = setInterval(() => {
+        setTimer((timer) => timer + 1)
+      }, 1000)
     }
-    function handlePause(){
-        clearInterval(countRef.current)
-        setIsPaused(false)
-    }
-    function handleResume(){
+    setIsPaused(false)
+  }
 
-    }
-    function handleReset(){
+  function handlePause() {
+    clearInterval(countRef.current)
+    setIsPaused(true)
+  }
 
-    }
+  function handleReset() {
+    clearInterval(countRef.current)
+    setTimer(0)
+    setIsPaused(true)
+  }
 }
 export default TomatenTimer
